7a6b868c2f3d84696d42d2f5109f7a083bdb148a
AtlasShd <tlsshrugged@gmail.com>
2021-12-06 21:18:32 +0300

    add adaptive tickets form and burger menu for 768px

--- js/burger.js
+++ js/burger.js
@@ -3,20 +3,26 @@ window.addEventListener('DOMContentLoaded', function () {
 	const navCrossBtn = document.querySelector('.nav__cross-btn'),
 		welcomeColumn = document.querySelector('.Welcome__column'),
 		navList = document.querySelector('.nav__list'),
-		nav = document.querySelector('.nav');
+		nav = document.querySelector('.nav'),
+		crossChecked = document.querySelector('.nav__cross');
 
 	function toggleBurger() {
 		navList.classList.toggle('show-burger');
 		welcomeColumn.classList.toggle('hide-column');
+		toggleBurgerCross();
+	}
+
+	function toggleBurgerCross() {
+		crossChecked.classList.toggle('nav__cross-checked');
 	}
 
 	document.body.addEventListener('click', function (e) {
-		if (e.target != navCrossBtn) {
+		if (e.target != navCrossBtn && e.target != navList) {
 			navList.classList.remove('show-burger');
 			welcomeColumn.classList.remove('hide-column');
+			crossChecked.classList.remove('nav__cross-checked');
 		}
 	});
-
 	navCrossBtn.addEventListener('click', toggleBurger);
 
 });
