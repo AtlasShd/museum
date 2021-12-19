dd95f1b261a2655c9a537338de83b38928e845c2
AtlasShd <tlsshrugged@gmail.com>
2021-12-19 09:12:18 +0300

    fixed bags

--- js/explore.js
+++ js/explore.js
@@ -1,7 +1,11 @@
 'use strict';
 
 window.addEventListener('DOMContentLoaded', function () {
-	document.querySelector('.slider input').addEventListener('input', function () {
+	document.querySelector('.slider input').addEventListener('input', function (e) {
+		e.preventDefault();
+
 		document.querySelector('.slider__before').style.width = this.value + '%';
 	});
+
+
 });
